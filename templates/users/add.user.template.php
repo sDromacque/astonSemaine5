65889d0f1619bcc15fdec22169643caa328f8a5e
Mathilde Drouin <mathilde.drouin59@gmail.com>
2017-01-06 11:36:39 +0100

    Layout template + pages templates

---
+++ templates/users/add.user.template.php
@@ -0,0 +1,25 @@
+<div class="user-add-container">
+    <nav class="sub-nav">
+        <h2>Add an user</h2>
+        <a href="" class="back">Manage users</a>
+    </nav>
+    <form action="">
+        <div class="form-group">
+            <label for="firstname">Firstname :</label>
+            <input type="text" id="firstname" class="firstname">
+        </div>
+        <div class="form-group">
+            <label for="lastname">Lastname :</label>
+            <input type="text" id="lastname" class="lastname">
+        </div>
+        <div class="form-group">
+            <label for="type">Type :</label>
+            <select name="type" id="type">
+                <option value="professor">Professor</option>
+                <option value="student">Student</option>
+                <option value="administrator">Administrator</option>
+            </select>
+        </div>
+        <button type="submit">Save</button>
+    </form>
+</div>
