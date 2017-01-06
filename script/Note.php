82af450c670d36648da436409b943d85f9880fa2
c-Dufour <duf.cyril@gmail.com>
2017-01-06 13:50:28 +0100

    Update Note.php

--- script/Note.php
+++ script/Note.php
@@ -1,9 +1,8 @@
 <?php
-
 require('Database.php');
 class Note
 {
-    public function postNote($note, $coeff, $person)
+    public static function postNote($note, $coeff, $person)
     {
         try {
             $query = $this->DBH->prepare("INSERT INTO note (note, coeff) VALUES (:note, :coeff, :person)");
@@ -12,11 +11,28 @@ class Note
                 'coeff' => $coeff,
                 'idPerson' => $person
             ));
-
             return true;
-
         } catch (PDOException $e) {
             exit($e->getMessage());
         }
+    }
+
+    public function getClassAvg($class)
+    {
+      try
+      {
+         return $this->DBH->query("SELECT AVG(note) FROM note INNER JOIN classeshavepersons ON note.idPerson = classeshavepersons.idPerson WHERE classeshavepersons.idClass = $class");
+      }catch(PDOException $e){
+        exit($e->getMessage());
+      }
+    }
+    public function getStudentAvg($student)
+    {
+      try
+      {
+         return $this->DBH->query("SELECT AVG(note) FROM note WHERE idPerson = $student");
+      }catch(PDOException $e){
+        exit($e->getMessage());
+      }
     }
 }
